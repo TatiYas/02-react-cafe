6bb5cc34baaef71b3fdfa2d131d53c57e7a287c4
TatiYas <iasinovska_lawyer@ukr.net>
2025-11-05 18:11:12 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+	plugins: [react()],
+	build: { 
+		sourcemap: true,
+		rollupOptions: {
+			external: ['react-modal']
+		}
+	},
+	resolve: {
+		alias: {
+			components: '/src/components',
+			styles: '/src/styles',
+			pages: '/src/pages',
+			api: '/src/api',
+			hooks: '/src/hooks',
+			routes: '/src/routes',
+			constants: '/src/constants',
+		}
+	}
+});
