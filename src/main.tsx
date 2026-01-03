9ee05c12bfecda50b8c21c95df6c83d8c20b4d6b
TatiYas <iasinovska_lawyer@ukr.net>
2026-01-03 02:38:15 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 
 
-import React from 'react';
-import { createRoot } from 'react-dom/client'
-import App from "./components/App/App.tsx"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize";
+import "./index.css";
+import App from "./components/App/App";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
